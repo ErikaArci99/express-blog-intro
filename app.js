067af89e7c1c437df732e0541f966aa5234b9b38
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-16 16:59:44 +0200

    [UPD] definita porta e rotta base

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require('express');
 
 // inizializziamo express in app
 const app = express();
+
+// definiamo la porta 
+const port = 3000;
+
+// definiamo la rotta base 
+app.get('/', (req, res) =>{
+    res.send('Server del mio blog')
+})
